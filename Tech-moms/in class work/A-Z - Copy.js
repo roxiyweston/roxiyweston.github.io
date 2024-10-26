a6a0a5c8656848b47b976006c84a7d3a4cf71047
roxiyweston <roxiyweston@gmail.com>
2024-10-26 10:43:39 -0600

    initial upload
     of portfolio and final for TechMoms

---
+++ Tech-moms/in class work/A-Z - Copy.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A</title>
+    {/* (AnimationEffect); */}
+    {/* <p>Acromantula, the biggest spider species found to date.
+        known for their ability to talk as well as their very valuble venom.;
+    </p> */}
+    {/* <img src="acrumantula" class="img-fluid rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt=">; */}
+
+
+</body>
+
+
+
+</html>
